f66f125a3dfe0c0f07f6e58786068b40c3044320
KetrinDarkDragon <76935641+KetrinDarkDragon@users.noreply.github.com>
2021-09-19 19:35:42 +0300

    Update ONEX dialogues extractor.bat

--- ONEX dialogues extractor.bat
+++ ONEX dialogues extractor.bat
@@ -1,10 +1,10 @@
-@echo off
+@echo 
 title JoD dialogue ONEX exporter
 if not exist ".\extracted" ( md ".\extracted")
 
 for %%A in (*.one) do ( 
 call ONEX.exe %%A  
-rename data.bin "%%~nA.bin" 
-move "%%~nA.bin" ".\extracted"
+rename data.bin "%%~nA.txt" 
+move "%%~nA.txt" ".\extracted"
 )
 del ref.bin
